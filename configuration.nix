a7dff061ee81787aa34d7ed03769c7e4c433715c
Lukas Bauza <LukasBauza.LB@proton.me>
2024-03-01 17:36:30 +0000

    hyprland

--- configuration.nix
+++ configuration.nix
@@ -51,6 +51,9 @@
 	pkgs.xterm
   ];
 
+	# Enable hyprland window manager.
+	programs.hyprland.enable = true;
+
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
