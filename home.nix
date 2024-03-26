1e94227118d1ba53915e9ff99ff3d25a70bbf92d
Lukas Bauza <LukasBauza.LB@proton.me>
2024-03-26 21:03:25 +0000

    home.nix

--- home.nix
+++ home.nix
@@ -52,7 +52,6 @@
     lazygit
     nodejs_21
     tree-sitter
-    vivaldi
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
