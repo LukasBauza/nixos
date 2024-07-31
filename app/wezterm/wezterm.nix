444a36b1b65f24c74ac3df60b472d343b7f3cb21
Lukas Bauza <LukasBauza.LB@proton.me>
2024-07-31 12:32:17 +0100

    Wezterm config fix

--- app/wezterm/wezterm.nix
+++ app/wezterm/wezterm.nix
@@ -3,18 +3,21 @@
 {
 	home.packages = [ pkgs.wezterm ];
 	programs.wezterm.enable = true;
-	programs.wezterm.extraConfig = ''
+
+	programs.wezterm.extraConfig = 
+		''
 		-- Pull in the wezterm API
 		local wezterm = require("wezterm")
 
 		-- This will hold the configuration.
 		local config = wezterm.config_builder()
 
-		-- This is where you actually apply your config choices.
-		
+		-- This is where you actually apply your config choices
+
+		-- For example, changing the color scheme:
 		config.color_scheme = "Kanagawa (Gogh)"
 
-		config.font = wezterm.fong("IntoneMono Nerd Font")
+		config.font = wezterm.font("IntoneMono Nerd Font")
 
 		config.use_fancy_tab_bar = true
 
@@ -31,5 +34,5 @@
 
 		-- and finally, return the configuration to wezterm
 		return config
-	'';
+		'';
 }
