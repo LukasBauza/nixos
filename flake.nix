328e3d27a24ca89d2b00f414941a26caaf9f8ddd
Lukas Bauza <lukasbauza.lb@proton.me>
2025-04-14 12:34:44 +0100

    Nix packages and home-manager version from 24.04 to 24.11

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
         nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
         home-manager = {
-            url = "github:nix-community/home-manager/release-24.05";
+            url = "github:nix-community/home-manager/release-24.11";
             # Makes sure that home-manager is the same version as nixpkgs.
             inputs.nixpkgs.follows = "nixpkgs";
         };
