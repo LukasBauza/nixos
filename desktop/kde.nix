53c9274192e9a305a064fd45e9f10eccb71b2e4b
Lukas Bauza <lukasbauza.lb@proton.me>
2025-05-24 09:50:47 +0100

    removed old desktop directory

--- desktop/kde.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.settings.General.Experimental = true;	# Needed for displaying battery percentage
-}
