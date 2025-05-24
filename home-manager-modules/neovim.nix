0d0e29179c04b5585ccff17fde96a7af45281695
Lukas Bauza <lukasbauza.lb@proton.me>
2025-05-24 13:32:10 +0100

    neovim config path added

--- home-manager-modules/neovim.nix
+++ home-manager-modules/neovim.nix
@@ -7,6 +7,12 @@
 
     config = lib.mkIf config.neovim.enable {
         #home.packages = [ pkgs.wl-clipboard ];
-        programs.neovim.enable = true;
+        programs.neovim = {
+            enable = true;
+            defaultEditor = true;
+            extraConfig = lib.fileContents ../../nvim/init.lua
+        }
+
+        environment.variables.EDITOR = "nvim";
     };
 }
