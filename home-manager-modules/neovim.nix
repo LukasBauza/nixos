d24131c491c00671b6deaad8eb0e6b7fdcf6cfcb
Lukas Bauza <lukasbauza.lb@proton.me>
2025-05-24 11:03:15 +0100

    syntax

--- home-manager-modules/neovim.nix
+++ home-manager-modules/neovim.nix
@@ -3,10 +3,10 @@
 {
     options = {
         neovim.enable = lib.mkEnableOption "enables neovim";
-    }
+    };
 
     config = lib.mkIf config.neovim.enable {
         home.packages = [ pkgs.neovim pkgs.wl-clipboard ];
         programs.neovim.enable = true;
-    }
+    };
 }
